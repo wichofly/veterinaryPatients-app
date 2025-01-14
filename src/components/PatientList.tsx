b74f410a83992d5de7732cecc2924b0b91924077
Mauricio Erazo <antonierazo13@hotmail.com>
2025-01-14 15:13:00 +0100

    feat: enhance PatientList component to display message when no patients are available

--- src/components/PatientList.tsx
+++ src/components/PatientList.tsx
@@ -4,7 +4,23 @@ const PatientList = () => {
   const { patients } = usePatientStore();
   console.log(patients);
 
-  return <div>PatientList</div>;
+  return (
+    <div className="">
+      {patients.length ? (
+        <p>Patient</p>
+      ) : (
+        <>
+          <h2 className="font-semibold text-3xl text-center">
+            There are not Patients
+          </h2>{' '}
+          <p className="text-xl mt-5 mb-10 text-center">
+            Patients Added{' '}
+            <span className="text-indigo-600 font-semibold">Here</span>
+          </p>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default PatientList;
