d0d6d72a6536790dd9d0110fb4b2eb90c92ff6f4
Mauricio Erazo <antonierazo13@hotmail.com>
2025-01-14 16:05:20 +0100

    refactor: PatientDetails component to use PatientDetailItem for displaying patient information

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -1,4 +1,5 @@
 import { Patient } from '../interfaces';
+import PatientDetailItem from './PatientDetailItem';
 
 interface PatientDetailsProps {
   patient: Patient;
@@ -7,18 +8,12 @@ interface PatientDetailsProps {
 const PatientDetails = ({ patient }: PatientDetailsProps) => {
   return (
     <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
-      <p className="font-semibold mb-3 text-zinc-800 uppercase">
-        ID: <span className="font-normal normal-case">{patient.id}</span>
-      </p>
-
-      <p className="font-semibold mb-3 text-zinc-800 uppercase">
-        Name: <span className="font-normal normal-case">{patient.name}</span>
-      </p>
-
-      <p className="font-semibold mb-3 text-zinc-800 uppercase">
-        Caretaker:{' '}
-        <span className="font-normal normal-case">{patient.caretaker}</span>
-      </p>
+      <PatientDetailItem label="ID" data={patient.id} />
+      <PatientDetailItem label="Name" data={patient.name} />
+      <PatientDetailItem label="Caretaker" data={patient.caretaker} />
+      <PatientDetailItem label="Email" data={patient.email} />
+      <PatientDetailItem label="Entry Date" data={patient.date.toString()} />
+      <PatientDetailItem label="Symptoms" data={patient.symptom} />
     </div>
   );
 };
